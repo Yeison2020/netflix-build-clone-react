408f459cd51d32d14f9ca4711da067671e689d16
Yeison <enmanuelquiterios@gmail.com>
2021-11-28 17:02:05 -0500

    Started Project first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBOJvquoZFvTK0dGqP4Jf4eC65FxuPRHaU",
+  authDomain: "nextflix-build-clone.firebaseapp.com",
+  projectId: "nextflix-build-clone",
+  storageBucket: "nextflix-build-clone.appspot.com",
+  messagingSenderId: "847733868667",
+  appId: "1:847733868667:web:90b7d203b2ae97e5473ade",
+};
