2a40f7d87af9d6a4229dc73dcef7f5ad178498cf
Yeison <enmanuelquiterios@gmail.com>
2021-11-28 17:46:35 -0500

    Adding logo and avatar

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Nav from "./Nav";
 
 const HomeScreen = () => {
-  return <div className="homeScreen"></div>;
+  return (
+    <div className="homeScreen">
+      <Nav />
+      <h1>Text</h1> <h1>Text</h1> <h1>Text</h1> <h1>Text</h1> <h1>Text</h1>{" "}
+      <h1>Text</h1> <h1>Text</h1> <h1>Text</h1> <h1>Text</h1> <h1>Text</h1>{" "}
+      <h1>Text</h1> <h1>Text</h1> <h1>Text</h1> <h1>Text</h1> <h1>Text</h1>{" "}
+      <h1>Text</h1> <h1>Text</h1> <h1>Text</h1> <h1>Text</h1> <h1>Text</h1>{" "}
+      <h1>Text</h1> <h1>Text</h1> <h1>Text</h1> <h1>Text</h1> <h1>Text</h1>{" "}
+      <h1>Text</h1> <h1>Text</h1> <h1>Text</h1> <h1>Text</h1> <h1>Text</h1>{" "}
+      <h1>Text</h1> <h1>Text</h1> <h1>Text</h1> <h1>Text</h1> <h1>Text</h1>{" "}
+      <h1>Text</h1> <h1>Text</h1> <h1>Text</h1> <h1>Text</h1> <h1>Text</h1>{" "}
+      <h1>Text</h1> <h1>Text</h1> <h1>Text</h1> <h1>Text</h1> <h1>Text</h1>
+    </div>
+  );
 };
 
 export default HomeScreen;
--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./nav.css";
 
 const Nav = () => {
-  return <div className="nav"></div>;
+  const [show, handleShow] = useState(false);
+
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  };
+  return (
+    <div className="nav nav_black">
+      <div className="nav_contents">
+        <img
+          className="nav_logo"
+          src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          alt="nextflix-log"
+        />
+        <img
+          className="nav_avatar"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          alt="avatarimage"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
