92ca46c66257eeb04d419f262752d02e2f53b784
Yeison <enmanuelquiterios@gmail.com>
2021-11-28 21:22:54 -0500

    Creating rows genres for the movies

--- src/Row.js
+++ src/Row.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./Row.css";
+import axios from "./axios";
 
-const Row = () => {
-  return <div></div>;
+const Row = ({ title, fetchUrl, isLargeRow = false }) => {
+  const [movie, setMovie] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+    </div>
+  );
 };
 
 export default Row;
