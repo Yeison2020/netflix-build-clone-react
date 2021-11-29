c34fb11c0b3f6cacb9a9b269722a56c7cc4d7faa
Yeison <enmanuelquiterios@gmail.com>
2021-11-28 19:08:25 -0500

    Added axios to fetch the data from  TMDB api

--- src/Banner.js
+++ src/Banner.js
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./banner.css";
 
 const Banner = () => {
-  return <div className="banner"></div>;
+  // This function truncate the amount of characters in the text
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {}, []);
+  const truncate = (string, n) => {
+    return string?.length > n ? string.substr(0, n - 1) + "..." : string;
+  };
+  return (
+    <header
+      className="banner"
+      style={{
+        background: "cover",
+        backgroundImage: `url('https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png')`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner_content">
+        <h1 className="banner_title">Movie Name</h1>
+        <div className="banner_buttons">
+          <button className="banner_button">Play</button>
+          <button className="banner_button">My List</button>
+          <h1 className="banner_description">
+            {truncate(`This is the test description`, 150)}
+          </h1>
+        </div>
+        <div className="banner--fadeBottom" />
+      </div>
+    </header>
+  );
 };
 
 export default Banner;
--- src/Nav.js
+++ src/Nav.js
@@ -24,7 +24,7 @@ const Nav = () => {
         <img
           className="nav_logo"
           src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
-          alt="nextflix-log"
+          alt="nextflix-logo"
         />
         <img
           className="nav_avatar"
