83a3a653e979a459223fa00a179e43d8c7deed25
Yeison <enmanuelquiterios@gmail.com>
2021-11-29 19:38:50 -0500

    Adding Sign up functionality

--- src/SignInScreen.js
+++ src/SignInScreen.js
@@ -3,11 +3,18 @@ import "./SignScreen.css";
 
 const SignInScreen = () => {
   return (
-    <div className="signScree">
+    <div className="signScreen">
       <form>
         <h1>Sign In</h1>
-        <input placeolder="Email" type="email" />
+        <input placeholder="Email" type="email" />
         <input placeholder="Password" type="Password" />
+        <button type="submit">Sign In</button>
+
+        <h4>
+          <span className="signUp_color_gray">New to Netflix? </span>{" "}
+          <span className="signupScreen_link">Sign Up</span>
+          now.
+        </h4>
       </form>
     </div>
   );
